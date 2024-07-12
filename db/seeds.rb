d843dccc2bd2141b37f3c3f3a11bb99adeec1c34
Michael Tuhaimer <michaeltuhaimer@Michaels-Air.localdomain>
2024-07-12 11:45:56 -0500

    Add model with seeds for initial data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Song.create(title: "When the Levee Breaks", album: "Led Zeppelin IV", artist: "Led Zeppelin", year: 1971)
+Song.create(title: "Ramble On", album: "Led Zeppelin II", artist: "Led Zeppelin", year: 1969)
+Song.create(title: "Ten Years Gone", album: "Physical Graffiti", artist: "Led Zeppelin", year: 1975)
